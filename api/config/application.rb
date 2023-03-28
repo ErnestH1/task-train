14e721c3b72630f3ee32da0dff52ed317199b999
ErnestH1 <hansonernest7@gmail.com>
2023-03-28 09:04:59 +0300

    middleware

--- api/config/application.rb
+++ api/config/application.rb
@@ -24,9 +24,9 @@ module Api
     config.load_defaults 7.0
 
     # cookies and session
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
-    config.action_dispatch.cookies_same_site_protection = :strict
+    config.middleware.use ActionDispatch::Cookies 
+    config.middleware.use ActionDispatch::Session::CookieStore 
+    config.action_dispatch.cookies_same_site_protection = :strict 
 
     # Configuration for the application, engines, and railties goes here.
     #
