04d64d7d43cb4f81cafed8211f2dc5b7b5568dcb
ErnestH1 <hansonernest7@gmail.com>
2023-03-28 09:01:43 +0300

    updated calculator.rb

--- api/app/models/calculator.rb
+++ api/app/models/calculator.rb
@@ -1,5 +1,5 @@
 class Calculator
-    attr_accessor :n1, :n2
+    attr_accessor :n1, :n2 
     def initialize(n1: ,n2:)
         @n1 = n1
         @n2 = n2
@@ -22,11 +22,11 @@ class Calculator
     end
 
     def square
-        @n1 * @n1
+        @n1 * @n1 
     end
 
     def cube
-        @n1 * @n1 * @n1
+        @n1 * @n1 * @n1 
     end
 
 end
