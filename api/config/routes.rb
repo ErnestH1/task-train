a7bedd34142a2d909ced6cbfa6d7941d0fd81145
ErnestH1 <hansonernest7@gmail.com>
2023-03-28 09:03:41 +0300

    updated routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
 
 
   # user
-  post '/users', to: 'users#register'
-  post '/users/login', to: 'users#login'
-  delete '/users/logout', to: 'users#logout'
+  post '/users', to: 'users#register' 
+  post '/users/login', to: 'users#login'  
+  delete '/users/logout', to: 'users#logout' 
   get '/user/login/check', to: 'users#check_login_status'
 
   # todos
   post '/todos', to: 'todos#create'
-  put '/todos/:id', to: 'todos#update'
-  delete '/todos/:id', to: 'todos#destroy'
+  put '/todos/:id', to: 'todos#update' 
+  delete '/todos/:id', to: 'todos#destroy' 
   get '/todos', to: 'todos#index'
 
   # verify auth
