7f55ad058fe890506288d8552ac954fd60f2aaa5
ErnestH1 <hansonernest7@gmail.com>
2023-03-28 09:05:39 +0300

    user controller

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     def register
         user = User.create(user_params)
         if user.valid?
-            save_user(user.id)
+            save_user(user.id) 
             app_response(message: 'Registration was successful', status: :created, data: user)
         else
             app_response(message: 'Something went wrong during registration', status: :unprocessable_entity, data: user.errors)
@@ -17,10 +17,10 @@ class UsersController < ApplicationController
         user = User.where(sql, { username: user_params[:username], email: user_params[:email] }).first
         if user&.authenticate(user_params[:password])
             save_user(user.id)
-            token = encode(user.id, user.email)
+            token = encode(user.id, user.email) 
             app_response(message: 'Login was successful', status: :ok, data: {user: user, token: token})
         else
-            app_response(message: 'Invalid username/email or password', status: :unauthorized)
+            app_response(message: 'Invalid username/email or password', status: :unauthorized) 
         end
     end
 
@@ -29,13 +29,13 @@ class UsersController < ApplicationController
         app_response(message: 'Logout successful')
     end
 
-    def check_login_status
+    def check_login_status 
         app_response(message: 'success', status: :ok)
     end
 
     private 
     
-    def user_params
+    def user_params 
         params.permit(:username, :email, :password)
     end
 
