1b11a2c07dbfd7352833fa059b76ebb97e1c100b
ErnestH1 <hansonernest7@gmail.com>
2023-04-11 10:50:58 +0300

    updated files

--- api/config/application.rb
+++ api/config/application.rb
@@ -27,7 +27,7 @@ module Api
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
     config.action_dispatch.cookies_same_site_protection = :strict
-    
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,23 +1,20 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  #user
+
+  # user
   post '/users', to: 'users#register'
   post '/users/login', to: 'users#login'
   delete '/users/logout', to: 'users#logout'
+  get '/user/login/check', to: 'users#check_login_status'
 
-  #todos
+  # todos
   post '/todos', to: 'todos#create'
-
-  #update todos
   put '/todos/:id', to: 'todos#update'
-  #delete todos
   delete '/todos/:id', to: 'todos#destroy'
-  #view all todos
   get '/todos', to: 'todos#index'
 
-
-
-  get '/user/login/check', to: 'users#check_login_status'
+  # verify auth
+  get '/verify', to: 'application#verify_auth'
 
 end
